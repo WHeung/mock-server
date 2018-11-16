224afbc5a18b53f23ec4b951a6763474ca9b5ab8
heung <liuweiheng@banggood.com>
2018-11-16 16:35:49 +0800

    CHG 页面渲染

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const fs = require('fs')
+const path = require('path')
 const Koa = require('Koa')
-const app = new Koa();
+const app = new Koa()
 const Router = require('koa-router')
 const router = new Router()
+const static = require('koa-static')
+const errorHandler = require('./errorHandler.js')
+
+app.use(static(
+  path.join(__dirname, './admin/build')
+))
 
 router.get('/admin', async (ctx, next) => {
-  const data = fs.readFileSync('./admin-angular/dist/admin-angular/index.html', 'utf-8')
+  const filePath = path.join(path.join(__dirname, './admin/build/index.html'))
+  const data = fs.readFileSync(filePath, 'utf-8')
   ctx.body = data
 })
 
 app
   .use(router.routes())
   .use(router.allowedMethods())
-// app.use(async ctx => {
-//   const request = ctx.request
-//   ctx.body = request.url
-//   if (request.url === '/admin') {
 
-//   }
-// })
+app.on('error', errorHandler)
 
 app.listen(3018)
